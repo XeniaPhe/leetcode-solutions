8f16277e230a19e3c6e4055729d3873007cf388a
XeniaPhe <m.tunahanbas90@gmail.com>
2026-04-05 16:49:21 +0300

    solved '3. Longest Substring Without Repeating Characters'

---
+++ 3. Longest Substring Without Repeating Characters/solution.go
@@ -0,0 +1,29 @@
+package longest_substring_without_repeating_characters
+
+func lengthOfLongestSubstring(s string) int {
+    lastSeen := make(map[rune]int, len(s))
+    longestRun := 0
+    currRunStart := 0
+
+    for i, chr := range s {
+        lastSeenIdx, seen := lastSeen[chr]
+
+        if seen && lastSeenIdx >= currRunStart {
+            currentRun := i - currRunStart
+            currRunStart = lastSeenIdx + 1
+
+            if currentRun > longestRun {
+                longestRun = currentRun
+            }
+        }
+
+        lastSeen[chr] = i
+    }
+
+    currentRun := len(s) - currRunStart
+    if currentRun > longestRun {
+        longestRun = currentRun
+    }
+
+    return longestRun
+}
