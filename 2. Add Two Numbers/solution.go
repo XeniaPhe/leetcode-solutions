54ea15b6a17d953068a37e346c5d69d5e98b38fa
XeniaPhe <m.tunahanbas90@gmail.com>
2026-04-05 15:45:48 +0300

    solved '2. Add Two Numbers'

---
+++ 2. Add Two Numbers/solution.go
@@ -0,0 +1,57 @@
+package add_two_numbers
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    resultNodes := make([]ListNode, 102)
+    currIdx := 0
+    head := &resultNodes[0]
+    current := head
+    carry := 0
+
+    for ; l1 != nil || l2 != nil; {
+        numLeft, numRight := 0, 0
+
+        if l1 != nil {
+            numLeft = l1.Val
+            l1 = l1.Next
+        }
+
+        if l2 != nil {
+            numRight = l2.Val
+            l2 = l2.Next
+        }
+
+        sum := numLeft + numRight + carry
+        carry = 0
+
+        if sum > 9 {
+            sum -= 10
+            carry = 1
+        }
+
+        currIdx += 1
+        current.Val = sum
+        current.Next = &resultNodes[currIdx]
+        current = current.Next
+    }
+
+    if carry != 0 {
+        currIdx += 1
+        current.Val = carry
+    }
+
+    resultNodes[currIdx - 1].Next = nil
+    return head
+}
