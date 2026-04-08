9b0125adef7397a6d956ddbde163e3675615b760
XeniaPhe <m.tunahanbas90@gmail.com>
2026-04-09 00:09:48 +0300

    solved '7. Reverse Integer'

---
+++ Medium/7. Reverse Integer/solution.go
@@ -0,0 +1,34 @@
+package reverse_integer
+
+/*
+Complexity:
+    Time Complexity: O(log(n))
+    Space Complexity: O(1)
+
+    log(n): Number of decimal digits in input
+*/
+
+func reverse(x int) int {
+    const max int32 = 214748364 // 2^31 / 10
+    num := int32(x) // int could be 64 bits, casting to 32 bits prevents even unintentional cheating
+
+    var sign int32 = 1
+    var reversed int32 = 0
+
+    if num < 0 {
+        reversed = -(num % 10)
+        num /= -10
+        sign = -1
+    }
+
+    for num != 0 {
+        if reversed > max {
+            return 0
+        }
+        
+        reversed = 10 * reversed + (num % 10)
+        num /= 10
+    }
+
+    return int(sign * reversed)
+}
