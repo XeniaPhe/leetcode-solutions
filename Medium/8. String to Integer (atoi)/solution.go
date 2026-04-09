355ab89657f11bedcec01cc6dce1807dda42e6b1
XeniaPhe <m.tunahanbas90@gmail.com>
2026-04-09 17:43:35 +0300

    solved '8. String to Integer (atoi)'

---
+++ Medium/8. String to Integer (atoi)/solution.go
@@ -0,0 +1,49 @@
+package string_to_integer
+
+import (
+    "strings"
+    "math"
+)
+
+/*
+Complexity:
+    Time Complexity           : O(n)
+    Total Space Complexity    : O(1)
+    Auxilary Space Complexity : O(1)
+
+    n: Size of the input
+*/
+
+func myAtoi(s string) int {
+    s = strings.Trim(s, " ")
+    if len(s) == 0 {
+        return 0
+    }
+
+    var sign int64 = 1
+    var max int64 = int64(math.MaxInt32)
+
+    // Some really unnecessary but beautiful ascii magic to get away with just one branch
+    if s[0] == '-' || s[0] == '+' {
+        diff := int64(s[0] - '+')
+        sign -= diff
+        max += diff / 2
+        s = s[1:]
+    }
+
+    var number int64 = 0
+    for i := 0; i < len(s); i++ {
+        chr := s[i]
+        if chr < '0' || chr > '9' {
+            break
+        }
+
+        number = 10 * number + int64(chr - '0')
+        if number >= max {
+            number = max
+            break
+        }
+    }
+
+    return int(sign * number)
+}
