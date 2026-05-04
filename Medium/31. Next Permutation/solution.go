2b9ac4fbdc273babc0bc8d83a0e36dd699e88437
XeniaPhe <m.tunahanbas90@gmail.com>
2026-05-04 23:24:53 +0300

    solved '31. Next Permutation'

---
+++ Medium/31. Next Permutation/solution.go
@@ -0,0 +1,27 @@
+package next_permutation
+
+import "slices"
+
+func nextPermutation(nums []int)  {
+    if len(nums) == 1 {
+        return
+    }
+
+    for min, i := 0, len(nums) - 1; i > 0; i -= 1 {
+        if min = i; nums[i - 1] >= nums[i] {
+            continue
+        }
+
+        for j := i + 1; j < len(nums); j += 1 {
+            if nums[j] > nums[i - 1] && nums[j] < nums[min] {
+                min = j
+            }
+        }
+
+        nums[i - 1], nums[min] = nums[min], nums[i - 1]
+        slices.Sort(nums[i:])
+        return
+    }
+
+    slices.Sort(nums)
+}
