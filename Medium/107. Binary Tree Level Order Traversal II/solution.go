a784406831968468c9b4cadc9f6d8bfc43bfbad5
XeniaPhe <m.tunahanbas90@gmail.com>
2026-06-06 15:45:44 +0300

    solved '107. Binary Tree Level Order Traversal II'

---
+++ Medium/107. Binary Tree Level Order Traversal II/solution.go
@@ -0,0 +1,36 @@
+package binary_tree_level_order_traversal_II
+
+import "slices"
+
+type TreeNode struct {
+    Val int
+    Left, Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+    if root == nil {
+        return nil
+    }
+    
+    queue, traversal := make([]*TreeNode, 1, 4), make([][]int, 1, 4)
+    queue[0], traversal[0] = root, make([]int, 1)
+
+    for depth, curr := 0, root; len(queue) > 0; depth += 1 {
+        for count, i := len(queue), 0; i < count; i += 1 {
+            if queue, curr = queue[1:], queue[0]; curr.Left != nil {
+                queue = append(queue, curr.Left)
+            }
+
+            if curr.Right != nil {
+                queue = append(queue, curr.Right)
+            }
+
+            traversal[depth][i] = curr.Val
+        }
+
+        traversal = append(traversal, make([]int, len(queue)))
+    }
+
+    slices.Reverse(traversal)
+    return traversal[1:]
+}
